9f03cca92d07d22d21f63853f256bdd3082a0c93
Dinosaur124 <dk26.01.1999@yandex.ru>
2023-11-10 12:42:21 +0300

    Добавила вывод роста и возраста

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             System.Console.WriteLine("Привет, динозаврик!");
+            int age = 5;
+            double height = 2.8;
+            Console.WriteLine($"Возраст: {age}  Рост: {height}м");
         }
     }
 }
