bab896dee930c633ed5b246ec409fd9b2580e117
Dinosaur124 <dk26.01.1999@yandex.ru>
2023-11-10 12:30:45 +0300

    Dino

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             System.Console.WriteLine("Привет, динозаврик!");
+            System.Console.WriteLine("Добро пожаловать в C#!");
+            System.Console.WriteLine("Пока мир...");
+            System.Console.WriteLine(24.5);
         }
     }
 }
